41fe2525a58862e1e5abbec80e796eddf25239d5
JewelRoam <2752594773@qq.com>
2026-08-10 17:08:16 +0800

    Initial Commit

---
+++ 在场/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  在场
+//
+//  Created by 郑恩嵘 on 2026/8/10.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 在场Tests/__Tests.swift
@@ -0,0 +1,18 @@
+//
+//  __Tests.swift
+//  在场Tests
+//
+//  Created by 郑恩嵘 on 2026/8/10.
+//
+
+import Testing
+
+struct __Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
